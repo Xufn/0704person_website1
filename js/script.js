b56ffcdf8acf0417c582af03bc6237cddfa9abda
mumaniu <luoyunqa@163.com>
2020-07-04 15:00:41 +0800

    0704

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if(this.scrollY > 20)
+      $(".navbar").addClass("sticky");
+    else
+      $(".navbar").removeClass("sticky");
+  });
+
+  $('.menu-toggler').click(function(){
+    $(this).toggleClass("active");
+    $(".navbar-menu").toggleClass("active");
+  });
+});
